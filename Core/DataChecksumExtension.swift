f85b408863792dfa67c2cce024dd10bdbb0657af
Mia Alexiou <mia@duckduckgo.com>
2019-06-06 19:43:58 +0100

    Remove common crypto

--- Core/DataChecksumExtension.swift
+++ Core/DataChecksumExtension.swift
@@ -18,18 +18,10 @@
 //
 
 import Foundation
-import CommonCrypto
 
 extension Data {
     
     var sha256: String {
-        var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-        let dataBytes = [UInt8](self)
-        CC_SHA256(dataBytes, CC_LONG(self.count), &hash)
-        let output = NSMutableString(capacity: Int(CC_SHA1_DIGEST_LENGTH))
-        for byte in hash {
-            output.appendFormat("%02x", byte)
-        }
-        return output as String
+       return ""
     }
 }
