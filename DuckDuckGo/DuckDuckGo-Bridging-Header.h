c13aaada1c7d45537f66d46f097b67383aa7fc28
Mia Alexiou <mia@duckduckgo.com>
2019-06-07 00:22:50 +0100

    Add bridging header for lottie

---
+++ DuckDuckGo/DuckDuckGo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  DuckDuckGo-Bridging-Header.h
+//  DuckDuckGo
+//
+//  Created by duckduckgo on 07/06/2019.
+//  Copyright © 2019 DuckDuckGo. All rights reserved.
+//
+
+#ifndef DuckDuckGo_Bridging_Header_h
+#define DuckDuckGo_Bridging_Header_h
+
+#import "LOTAnimationView.h"
+#endif /* DuckDuckGo_Bridging_Header_h */
